bb7b8640a0c1886f25f71106e023ba47e68e04a6
Boroncarbon <joseph.m.parkinson@gmail.com>
2026-03-01 15:15:28 +0000

    Working 8 bit encoding

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from ASCII import binary, string
-s = binary("Hello, world!")
+
+test_string = "Hello, world!"
+
+with open("output_uncompressed.txt", "w") as f:
+    f.write(test_string)
+
+s = binary(test_string)
+
+with open("output_binary_string.txt", "w") as f:
+    f.write(s)
+
 bytess = int(s, base=2).to_bytes(len(s) // 8, byteorder='big')
-
-with open("output.txt", "w") as f:
-    f.write(binary("Hello, world!"))
-
-with open("output.bin", "wb") as f:
+with open("output_raw_binary_8_bit.txt", "wb") as f:
     f.write(bytess)
 
-with open("output.bin", "rb") as f:
+with open("output_raw_binary_8_bit.txt", "rb") as f:
     raw = f.read()
 
 bits = ''.join(f'{b:08b}' for b in raw)
